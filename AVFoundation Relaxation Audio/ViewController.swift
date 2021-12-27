54a47c3b894954c5344589902d4bacb3f602743a
faisal alfariheedi <falfariheedi@gmail.com>
2021-12-27 21:05:01 +0300

    fin

--- AVFoundation Relaxation Audio/ViewController.swift
+++ AVFoundation Relaxation Audio/ViewController.swift
@@ -6,14 +6,75 @@
 //
 
 import UIKit
+import AVFoundation
+import BAFluidView
+import SpriteKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    @IBOutlet weak var playstop: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var vi: UIView!
+    var audio = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main.path(forResource: "m1", ofType: "wav")! ))
+    var viewbaf : BAFluidView!
+    var slidesync:Timer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewbaf = BAFluidView(frame: vi.frame, startElevation: 0.5)
+        audio.delegate = self
+        slidesync = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.sliderupdate), userInfo: nil, repeats: true)
+        viewbaf.strokeColor = UIColor.white
+        viewbaf.fillColor = UIColor.blue
+        viewbaf.keepStationary()
+        viewbaf.startAnimation()
+//        viewbaf.maxAmplitude = Int(vi.frame.height)
+        self.view.insertSubview(viewbaf, aboveSubview: self.vi)
+         
+        UIView.animate(withDuration: 0.5, animations: { [self] in
+        viewbaf.alpha=1.0
+        }, completion: nil)
     }
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        audio.play()
+    
+    }
+    
 
-
+    @IBAction func editend(_ sender: UISlider) {
+        audio.play(atTime: TimeInterval(Float(audio.duration) * slider.value))
+//         for some reason the audio doesn`t want to start at a //specific time
+        slidesync = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.sliderupdate), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func edend(_ sender: UISlider) {
+        audio.play(atTime: TimeInterval(Float(audio.duration) * slider.value))
+        print(TimeInterval(Float(audio.duration) * slider.value))
+        slidesync = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.sliderupdate), userInfo: nil, repeats: true)
+    }
+    @IBAction func edstart(_ sender: UISlider) {
+        audio.stop()
+        slidesync.invalidate()
+        print(audio.duration)
+    }
+    
+    @IBAction func editstart(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func playstopmeth(_ sender: UIButton) {
+        if(audio.isPlaying){
+            audio.stop()
+            print("stopped")
+        }else{
+            audio.play()
+            print("playing")
+        }
+    }
+    @objc func sliderupdate(){
+        if(audio.isPlaying){
+        slider.value = Float(audio.currentTime)/Float(audio.duration)
+        }
+        viewbaf.fill(to: NSNumber(value: Float(audio.currentTime)/Float(audio.duration)))
+    }
 }
 
